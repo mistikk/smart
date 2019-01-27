eb1bf102f62aafbea29086d9269f6abfae0cef31
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-01-27 22:49:17 +0300

    Added cache feature

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,20 @@
 import api from '../config/api';
 
 export const login = data => new Promise((resolve, reject) => {
-  api
-    .post('/auth', data)
-    .then((res) => {
-      api.defaults.headers.common.authtoken = res.data.data.token;
-      resolve(res.data.data);
-    })
-    .catch((error) => {
-      reject(error);
-    });
+  const token = localStorage.getItem('token');
+  if (token) {
+    api.defaults.headers.common.authtoken = token;
+    resolve(token);
+  } else {
+    api
+      .post('/auth', data)
+      .then((res) => {
+        api.defaults.headers.common.authtoken = res.data.data.token;
+        localStorage.setItem('token', res.data.data.token);
+        resolve(res.data.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  }
 });
--- src/services/product.js
+++ src/services/product.js
@@ -1,12 +1,23 @@
 import api from '../config/api';
 
 export const getProductByCode = id => new Promise((resolve, reject) => {
-  api
-    .get(`/products/get-product-by-code/${id}`)
-    .then((res) => {
-      resolve(res.data.data);
-    })
-    .catch((error) => {
-      reject(error);
-    });
+  const product = localStorage.getItem(id);
+  const now = new Date();
+
+  if (product && JSON.parse(product).remove_from_cache > now) {
+    resolve(JSON.parse(product));
+  } else {
+    api
+      .get(`/products/get-product-by-code/${id}`)
+      .then((res) => {
+        localStorage.setItem(id, JSON.stringify({ ...res.data.data, remove_from_cache: now.setHours(now.getHours() + 1) }));
+        resolve(res.data.data);
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem('token');
+        }
+        reject(error);
+      });
+  }
 });
